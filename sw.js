b957c5f8c227c0bf32cd3a4dc163c67bb81d7c83
imamovelay0-hue <ezizeimamova5@gmail.com>
2026-07-27 20:09:37 +0000

    Google Analytics elave edildi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zirve-v4';
+const CACHE = 'zirve-v5';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
